ef1ca057b926de2e79f66c7685b5f258efc8ce6b
ayang4-byte <ayang4@hwemail.com>
2026-07-27 15:38:57 -0700

    Train more changes

--- train.py
+++ train.py
@@ -1,14 +1,14 @@
 import torch
-from torch import nn
-from torch.utils.data import DataLoader
+from torch import nn, optim
+from torch.utils.data import Dataset, DataLoader
 from torchvision import datasets
 from torchvision.transforms import v2 #For images
 import numpy as np
 
 class MainDataset(Dataset):
     def __init__(self, features, labels):
-        self.features = torch.tensor(features, dtype=torch.float32)
-        self.labels = torch.tensor(labels, dtype=torch.long)
+        self.features = torch.tensor(features, dtype=torch.long)
+        self.labels = torch.tensor(labels, dtype=torch.float32)
 
     def __len__(self):
         return len(self.features)
@@ -16,16 +16,17 @@ class MainDataset(Dataset):
     def __getitem__(self, index):
         return self.features[index], self.labels[index]
 
-loss_function = nn.CrossEntropyLoss()
+constellation = Constellation().to(device)
+decoder = Decoder().to(device)
+loss_function = nn.MSELoss()
 optimizer = optim.Adam(list(constellation.parameters()) + list(decoder.parameters()), lr = 0.001) #lr is essentially how big of an adjustment we want at the beginning
 
 with open("list_of_bits.txt","r") as file:
     all_symbols = []
     for line in file:
-        line_list = []
+        bits = []
         for bit in line.strip(): #line.strip removes \n at the end
-            line_list.append(bit)
-        bits.append(line_list)
+            bits.append(int(bit))
         symbols = bits_to_symbol_indices(bits, 8) #Converts the bits in groups of 3 to the INDEX of the symbol (0-7)
         all_symbols.extend(symbols)
 all_symbols = np.array(all_symbols, dtype = np.uint8)
@@ -43,16 +44,17 @@ def trainer(dataloader, constellation, decoder, optimizer, loss_function):
         # returns the amplitudes of all our symbols (they are currently in all the indices)
         transmitted = constellation(symbol_indices)
 
-        # AWGN channel
-        noise = 0.1 * torch.randn_like(transmitted)
+        # # AWGN channel
+        noise = torch.zeros_like(transmitted) # 0.1 * torch.randn_like(transmitted)
         received = transmitted + noise
 
         # Neural decoder
-        logits = decoder(received)
-        loss = loss_function(logits, labels)
+        prediction = decoder(received)
+        loss = loss_function(prediction, labels)
 
         optimizer.zero_grad()
         loss.backward()
         optimizer.step()
         if batch % 50 == 0:
             print(loss.item())
+            print(constellation.normalized_points())
