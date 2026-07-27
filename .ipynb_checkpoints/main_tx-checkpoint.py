7d1b5071d63e88c51171d98384317b45d84f008e
ayang4-byte <ayang4@hwemail.com>
2026-07-27 09:50:38 -0700

    files

---
+++ .ipynb_checkpoints/main_tx-checkpoint.py
@@ -0,0 +1,43 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import scipy.signal as signal
+import time 
+import sys
+
+from cosmos import *
+from digicomm import *
+
+import adi
+
+# Directory for saving plots
+dir_plots = 'plots/'
+
+# ---------------------------------------------------------------
+# Setup.
+# --------------------------------------3-------------------------
+sdr_tx = adi.Pluto("usb:1.12.5")
+
+tx = PlutoTransmitter()
+tx.set_sdr(sdr_tx)
+tx.set_channel(9)
+tx.set_power_level(75)
+
+# ---------------------------------------------------------------
+# Generate random symbols.
+# ---------------------------------------------------------------
+num_pam_symbols = 200 # number of random data symbols to generate
+tx_symbols = 2*np.random.randint(0,2,num_pam_symbols) - 1
+tx_symbols = np.real(tx_symbols)
+
+# ---------------------------------------------------------------
+# Transmit.
+# ---------------------------------------------------------------
+tx.transmit(tx_symbols)
+
+while True:
+    print("Transmitting...")
+    time.sleep(10)
+
+
+
+
